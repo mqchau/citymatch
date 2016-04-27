35ca1a31e8fb127f5242ea5bf98f0d650c66511f
mqchau <quanqchau@gmail.com>
2016-04-27 16:05:41 -0700

    use spark and saved some time comparing to sequential execution

--- rate_city.py
+++ rate_city.py
@@ -2,6 +2,7 @@ import pprint
 import os.path
 import json
 import time
+from pyspark import SparkContext, SparkConf
 
 all_cities = None
 
@@ -27,17 +28,38 @@ def rate_city(occupation, city_char):
     return top_cities_with_char_rate
 
 def rate_one_city(city_obj, city_char):
-    return city_obj["cost"]
+    return (city_obj["name"] + ", " + city_obj["state"], city_obj["cost"])
 
 if __name__ == "__main__":
-    load_data_to_memory()
+    # load_data_to_memory()
 
-    pp = pprint.PrettyPrinter(indent=4)
-    a = rate_city("Sales Associate", {
+    city_char = {
         "temp_low" : 40,
         "temp_high" : 120,
         "precip_low" : 0,
         "precip_high" : 20,
         "settle_type": "urban"
-    })
+    }
+
+
+    pp = pprint.PrettyPrinter(indent=4)
+    # a = rate_city("Sales Associate", {
+    #     "temp_low" : 40,
+    #     "temp_high" : 120,
+    #     "precip_low" : 0,
+    #     "precip_high" : 20,
+    #     "settle_type": "urban"
+    # })
+
+    conf = SparkConf().setAppName("rate_city").setMaster("local[4]")
+    sc = SparkContext(conf=conf)
+    lines = sc.textFile(os.path.join("datasource", "all_cities_data.json"), 4)
+
+    start_time = time.time()
+    all_cities = lines.map(lambda x: rate_one_city(json.loads(x), city_char)).top(2, lambda x: x[1])
+    print("--- finished in %s seconds ---" % (time.time() - start_time))
+
+    all_cities_result = all_cities
+
+    pp.pprint(all_cities_result)
 
