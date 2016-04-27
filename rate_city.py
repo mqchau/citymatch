762f16f95d7c7ca7414343a844e2670e3c1330c4
mqchau <quanqchau@gmail.com>
2016-04-27 15:54:46 -0700

    got function to see how fast rating all cities is

--- rate_city.py
+++ rate_city.py
@@ -1,4 +1,14 @@
 import pprint
+import os.path
+import json
+import time
+
+all_cities = None
+
+def load_data_to_memory():
+    global all_cities
+    with open(os.path.join("datasource", "all_cities_data.json"), "r") as f:
+        all_cities = map(lambda x: json.loads(x), f.readlines())
 
 # --------------------------------------------------
 # Find cities with best matches to the char and highest ratio of salaray / cost
@@ -10,19 +20,24 @@ import pprint
 #        And expected salary and cost
 # --------------------------------------------------
 def rate_city(occupation, city_char):
-    pass
+    start_time = time.time()
+    # all_cities_with_char_rate = map(lambda x: rate_one_city(x, city_char), all_cities)
+    top_cities_with_char_rate = sorted(all_cities, key=(lambda x: rate_one_city(x, city_char)), reverse=True)
+    print("--- finished in %s seconds ---" % (time.time() - start_time))
+    return top_cities_with_char_rate
 
-
+def rate_one_city(city_obj, city_char):
+    return city_obj["cost"]
 
 if __name__ == "__main__":
+    load_data_to_memory()
+
     pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(rate_city("Analyst", {
+    a = rate_city("Sales Associate", {
         "temp_low" : 40,
         "temp_high" : 120,
         "precip_low" : 0,
         "precip_high" : 20,
-        "humid_low": 50,
-        "humid_high": 80,
         "settle_type": "urban"
-    }))
+    })
 
